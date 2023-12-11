b5c3addda11bf21d1f23cf784b56871b0b113428
DraconDev <dracsharp@gmail.com>
2023-12-11 00:57:31 +0000

    upd login

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,7 +1,12 @@
 type Props = {};
+import { CgProfile } from "react-icons/cg";
 
 const Login = (props: Props) => {
-    return <div>Login</div>;
+    return (
+        <div className="flex items-center  text-lg text-accent">
+            <CgProfile className="w-9 h-9" />
+        </div>
+    );
 };
 
 export default Login;
