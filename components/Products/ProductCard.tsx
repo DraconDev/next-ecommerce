8db03bbc551763cf1f09bafa0ca543ae9feac453
DraconDev <dracsharp@gmail.com>
2023-12-11 01:46:44 +0000

    upd product card

--- components/Products/ProductCard.tsx
+++ components/Products/ProductCard.tsx
@@ -1,7 +1,19 @@
 import { Product } from "@/types/types";
+import Image from "next/image";
 
 const ProductCard = ({ product }: { product: Product }) => {
-    return <div>ProductCard</div>;
+    return (
+        <div className="flex flex-col w-[255px] h-full max-h-[630px] items-center justify-center text-center">
+            <Image
+                src={product.image}
+                alt={product.title}
+                width={255}
+                height={255}
+            />
+            <p className="">{product.title}</p>
+            <p className="">{product.description}</p>
+        </div>
+    );
 };
 
 export default ProductCard;
