29c8a3da146a8d0b3b97b279606893ff7ad60acf
DraconDev <dracsharp@gmail.com>
2023-12-11 01:12:51 +0000

    style menu

--- components/Navbar/SecondRow/Menu.tsx
+++ components/Navbar/SecondRow/Menu.tsx
@@ -1,7 +1,12 @@
 type Props = {};
+import { IoMenu } from "react-icons/io5";
 
 const Menu = (props: Props) => {
-    return <div>Menu</div>;
+    return (
+        <div>
+            <IoMenu className="w-8 h-8" />
+        </div>
+    );
 };
 
 export default Menu;
