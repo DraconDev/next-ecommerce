fc62a57e661abd8dd719d27862e8f24c18d4254f
DraconDev <dracsharp@gmail.com>
2023-12-11 04:35:27 +0000

    fix menu position

--- components/Navbar/SecondRow/NavbarSecondRow.tsx
+++ components/Navbar/SecondRow/NavbarSecondRow.tsx
@@ -6,7 +6,7 @@ type Props = {};
 const NavbarSecondRow = (props: Props) => {
     return (
         <div className="bg-tertiary w-full p-1  text-xl justify-center flex">
-            <div className="max-w-6xl flex gap-2 w-full ">
+            <div className="max-w-6xl flex gap-2 w-full items-center">
                 <Menu />
                 <CategoryList />
             </div>
