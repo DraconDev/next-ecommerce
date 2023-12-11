41d80cf6db63c564de37d19b10a0ee4a9bcc5d21
DraconDev <dracsharp@gmail.com>
2023-12-11 00:56:19 +0000

    upd basket

--- components/Basket.tsx
+++ components/Basket.tsx
@@ -1,7 +1,13 @@
 type Props = {};
+import { BsBasket3 } from "react-icons/bs";
 
 const Basket = (props: Props) => {
-    return <div>Basket</div>;
+    return (
+        <div className="flex items-center gap-1 text-lg text-accent">
+            <BsBasket3 className="w-9 h-9" />
+            <span className=""></span>
+        </div>
+    );
 };
 
 export default Basket;
