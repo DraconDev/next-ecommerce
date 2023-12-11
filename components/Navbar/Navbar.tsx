a4a58f7682547360b7bfcf540ff0a4269ae7be8f
DraconDev <dracsharp@gmail.com>
2023-12-11 04:00:38 +0000

    style navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -9,7 +9,7 @@ import NavbarSecondRow from "./SecondRow/NavbarSecondRow";
 const Navbar = (props: Props) => {
     return (
         <div className="z-20 fixed top-0 left-0 text-primary bg-secondary w-full md:text-3xl flex flex-col justify-center items-center">
-            <div className="w-full flex p-1 px-3 items-center max-w-6xl gap-3">
+            <div className="w-full flex py-1  items-center max-w-6xl gap-3 justify-between">
                 <SiTurbosquid className="w-12 h-12" />
                 <SearchField />
                 <div className="flex gap-2">
