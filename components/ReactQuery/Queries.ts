1ecebd243f87ec74f8d669f923375f2a86a73b73
DraconDev <dracsharp@gmail.com>
2023-12-12 00:17:17 +0000

    add react query

---
+++ components/ReactQuery/Queries.ts
@@ -0,0 +1,9 @@
+// import { queryOptions } from "@tanstack/react-query";
+
+// function groupOptions(id: number) {
+//     return queryOptions({
+//         queryKey: ["groups", id],
+//         queryFn: () => {},
+//         // staleTime: 5 * 1000,
+//     });
+// }
