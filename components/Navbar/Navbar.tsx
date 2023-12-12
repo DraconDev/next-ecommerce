ae8524d1ff1a2052d4f22a6f828b8b38668e91c8
DraconDev <dracsharp@gmail.com>
2023-12-12 06:12:55 +0000

    upd z of nav

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -9,7 +9,7 @@ import NavbarSecondRow from "./SecondRow/NavbarSecondRow";
 
 const Navbar = (props: Props) => {
     return (
-        <div className="z-20 fixed top-0 left-0 text-primary bg-secondary w-full md:text-3xl flex flex-col justify-center items-center p">
+        <div className="z-30 fixed top-0 left-0 text-primary bg-secondary w-full md:text-3xl flex flex-col justify-center items-center p">
             <div className="w-full flex   items-center max-w-6xl gap-2 justify-between p-1">
                 <Link href={"/"}>
                     <SiTurbosquid className="w-12 h-12" />
