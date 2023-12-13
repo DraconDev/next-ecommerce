8c2e3e64addb0153bcec03a3a45a406efb65d58b
DraconDev <dracsharp@gmail.com>
2023-12-13 07:37:18 +0000

    add checkout list with map

--- components/Checkout/CheckoutItems.tsx
+++ components/Checkout/CheckoutItems.tsx
@@ -1,22 +1,31 @@
 "use client";
 import { basketItems } from "@/state/jotai";
+import { BasketItemType } from "@/types/types";
 import { useAtom } from "jotai";
 
 type Props = {};
 
 const CheckoutItems = (props: Props) => {
-    const [basket, _] = useAtom(basketItems);
+    const [basket, _] = useAtom<Map<number, BasketItemType>>(basketItems);
+
     return (
-        <div>
-            {/* {basket.map((item, index) => (
+        <div className="w-full gap-3 flex flex-col p-2">
+            {/* Iterate over the values of the basket map */}
+            {Array.from(basket.values()).map((item, index) => (
                 <div
                     key={index}
-                    className="flex justify-between text-xl w-2/3"
+                    className="flex justify-between text-xl w-full"
                 >
-                    <p>{item.title}</p>
-                    <p className="">{`$${item.price}`}</p>
+                    {/* Access properties from the BasketItemType */}
+                    <p className="flex flex-wrap w-1/2 ">
+                        {item.product.title}
+                    </p>
+                    <div className="flex justify-between gap-4 w-1/6">
+                        <div className="flex ">{`$${item.product.price}`}</div>
+                        <div className="">{item.quantity}</div>
+                    </div>
                 </div>
-            ))} */}
+            ))}
         </div>
     );
 };
