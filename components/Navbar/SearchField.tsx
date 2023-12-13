d3a3431af0f87f5bd8f01732c52b4b49abe58e93
DraconDev <dracsharp@gmail.com>
2023-12-13 03:18:28 +0000

    style search selector

--- components/Navbar/SearchField.tsx
+++ components/Navbar/SearchField.tsx
@@ -67,7 +67,7 @@ const SearchField = (props: Props) => {
                     ref={dropdownRef}
                 >
                     {dropMenuState && (
-                        <div className="bg-white text-black rounded-lg  flex flex-col absolute top-3 left-[-10px] p-1 border-2 border-primary ">
+                        <div className="bg-white text-black rounded-lg  flex flex-col absolute top-3 left-[-10px] p-1 border-2 border-secondary ">
                             <DropdownOption
                                 option="titles"
                                 setSearchSetting={setSearchSetting}
