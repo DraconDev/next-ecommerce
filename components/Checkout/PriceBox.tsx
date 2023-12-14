26664b7aa9f263da1b42e7eea40e00fc7eb7ed63
DraconDev <dracsharp@gmail.com>
2023-12-14 05:28:14 +0000

    add price to pricebox

--- components/Checkout/PriceBox.tsx
+++ components/Checkout/PriceBox.tsx
@@ -1,7 +1,18 @@
+"use client";
+
+import { itemsCost } from "@/state/jotai";
+import { useAtom } from "jotai";
+
 type Props = {};
 
 const PriceBox = (props: Props) => {
-    return <div>PriceBox</div>;
+    const [price, _] = useAtom(itemsCost);
+    return (
+        <div className="flex flex-row justify-between">
+            <div className="">Total: </div>
+            <div className="">${price.toFixed(2)}</div>
+        </div>
+    );
 };
 
 export default PriceBox;
