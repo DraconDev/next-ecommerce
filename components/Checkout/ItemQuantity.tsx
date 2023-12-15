087d199e4ef01357be4ea17f2e981c8c9932e2fe
DraconDev <dracsharp@gmail.com>
2023-12-15 10:07:24 +0000

    upd itemquantity to use helper

--- components/Checkout/ItemQuantity.tsx
+++ components/Checkout/ItemQuantity.tsx
@@ -1,43 +1,22 @@
+import { useHandleClickOutside } from "@/helpers/click/clickoutside";
 import { useUpdateItem } from "@/helpers/shop/shop";
 import { BasketItemType } from "@/types/types";
-import { useEffect, useRef, useState } from "react";
 import { IoIosArrowDropdown } from "react-icons/io";
 
 type Props = {};
 
 const ItemQuantity = ({ item }: { item: BasketItemType }) => {
     const updateQuantity = useUpdateItem();
-    const [dropdownState, setDropdownState] = useState(false);
+
     const amount = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-    const dropdownRef = useRef<HTMLDivElement>(null);
-    const buttonRef = useRef<HTMLButtonElement>(null);
-
-    const handleClickOutside = (event: MouseEvent) => {
-        // Close the dropdown if the click is outside of it and not on the button
-        if (
-            dropdownRef.current &&
-            buttonRef.current &&
-            !dropdownRef.current.contains(event.target as Node) &&
-            !buttonRef.current.contains(event.target as Node)
-        ) {
-            setDropdownState(false);
-        }
-    };
-
-    useEffect(() => {
-        document.addEventListener("mousedown", handleClickOutside);
-
-        return () => {
-            document.removeEventListener("mousedown", handleClickOutside);
-        };
-    }, []);
+    const { dropdownRef, buttonRef, dropMenuState, setDropMenuState } =
+        useHandleClickOutside();
 
     return (
         <div className="relative">
             <button
                 className="flex items-center gap-1 "
-                onClick={() => setDropdownState(!dropdownState)}
+                onClick={() => setDropMenuState(!dropMenuState)}
                 ref={buttonRef}
             >
                 <div className="">{item.quantity}</div>
@@ -45,9 +24,9 @@ const ItemQuantity = ({ item }: { item: BasketItemType }) => {
                     <IoIosArrowDropdown className="w-6 h-6" />
                 </div>
             </button>
-            {dropdownState && (
+            {dropMenuState && (
                 <div
-                    className="absolute top-10 right-0 bg-primary border-2 rounded-lg  p-1"
+                    className="absolute top-10 right-0 bg-primary border-2 rounded-lg  p-1 z-20"
                     ref={dropdownRef}
                 >
                     {amount.map((amount) => (
