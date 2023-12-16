9ce448a492947e7c002b4ff05dfb294af4b22c22
DraconDev <dracsharp@gmail.com>
2023-12-16 11:48:38 +0000

    update orders

--- app/orders/[[...all]]/page.tsx
+++ app/orders/[[...all]]/page.tsx
@@ -1,17 +1,20 @@
 "use client";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 type Props = {};
 
 const Orders = (props: Props) => {
     const router = useRouter();
-    // useEffect(() => {
-    //     const timer = setTimeout(() => {
-    //         // The action you want to perform after 3 seconds
-    //         router.push("/");
-    //     }, 3000);
-    //     return () => clearTimeout(timer);
-    // }, []);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            // The action you want to perform after 3 seconds
+            router.push("/");
+        }, 2000);
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <div className="flex justify-center w-full h-full items-center flex-col">
             <div className="text-2xl">Order Successful</div>
