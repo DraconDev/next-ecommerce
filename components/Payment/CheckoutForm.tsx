e6699677bc5ce0d2a4299c4f18e3db5131a1dd83
DraconDev <dracsharp@gmail.com>
2023-12-16 10:40:09 +0000

    style payment

--- components/Payment/CheckoutForm.tsx
+++ components/Payment/CheckoutForm.tsx
@@ -87,8 +87,8 @@ export default function CheckoutForm() {
     };
 
     return (
-        <div className="">
-            <div className="mb-4">
+        <div className="flex flex-col p-2 ">
+            <div className="p-1 text-sm">
                 {IS_TEST_MODE
                     ? "Use this card to test: " + TEST_CARD_NUMBER
                     : ""}
@@ -96,7 +96,7 @@ export default function CheckoutForm() {
             <form
                 id="payment-form"
                 onSubmit={handleSubmit}
-                className="bg-white p-4 rounded-lg gap-2 flex flex-col"
+                className="bg-white p-4 rounded-lg gap-3 flex flex-col"
             >
                 <PaymentElement
                     id="payment-element"
@@ -106,7 +106,7 @@ export default function CheckoutForm() {
                     <button
                         disabled={isLoading || !stripe || !elements}
                         id="submit"
-                        className="p-2 bg-slate-200 rounded h-10 w-1/2                        "
+                        className="p-2 bg-accent font-bold rounded h-10 w-full                        "
                     >
                         <span id="button-text">
                             {isLoading ? "Processing..." : "Pay now"}
